33b9197d654de93415102af12c4309f577e66da9
asalazar <a.n.salazar@tudelft.nl>
2018-04-23 13:26:44 +0200

    update

--- src/main/scala/cli/Ptolemy.scala
+++ src/main/scala/cli/Ptolemy.scala
@@ -11,19 +11,27 @@ object Ptolemy {
 
   def main(args: Array[String]): Unit = {
     val help = (
-      "Usage: java -jar Pandora.jar [module]\n\n" +
+      "Usage: java -jar ptolemy.jar [module]\n\n" +
+        "CANONICAL QUIVER CONSTRUCTION\n" +
         "extract                      Run Ptolemy's extract module.\n"+
         "syntenic-anchors             Compute syntenic anchors from pairwise ORF alignments.\n"+
-        "construct-hlgg               Construct HLGG.\n"
+        "canonical-quiver             Construct canonical quiver.\n\n" +
+        "STRUCTURAL VARIANT CALLING\n" +
+        "variant-calling              Identify structural variants as a population using maximally-labelled paths\n\n"+
+        "OPTIONAL\n" +
+        "run-msa                      Induce MSA across syntenic anchors.\n"
       )
 
     if (args.length == 0) {
       println(help)
     } else {
       args(0) match {
-        case "extract"             => extract_module.Extract.main(args.drop(1))
-        case "syntenic-anchors"    => cluster_module.SyntenicAnchors.main(args.drop(1))
-        case "construct-hlgg"      => construct.HLGG.main(args.drop(1))
+        case "extract"             => build_db.Extract.main(args.drop(1))
+        case "syntenic-anchors"    => syntenic_anchors.SyntenicAnchors.main(args.drop(1))
+        case "canonical-quiver"    => canonical_quiver.ConstuctCanonicalQuiver.main(args.drop(1))
+        case "run-msa"             => msa.RunMSA.main(args.drop(1))
+        case "variant-calling"     => variant_calling.HighLevelVariantCaller.main(args.drop(1))
+        case "parents"             => parental_perspective.CharacterizeWithParents.main(args.drop(1))
         case _                     => println(help)
       }
     }
